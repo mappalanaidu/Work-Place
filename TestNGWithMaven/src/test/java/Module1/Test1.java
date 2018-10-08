10be4c6a2528de9f883be1faf5a8f6179937a6b8
Naidu <Naidu@sathvik>
2018-10-08 16:01:46 +0530

    TestNG with Maven Project

---
+++ TestNGWithMaven/src/test/java/Module1/Test1.java
@@ -0,0 +1,11 @@
+package Module1;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+  @Test
+  public void f() 
+  {
+	  System.out.println("This is sample Test from TestNG with Maven");
+  }
+}
