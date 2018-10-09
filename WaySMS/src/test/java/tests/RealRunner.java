f57f4cf5c2bf2b3e10e04432756afe8802d16f39
Naidu <Naidu@sathvik>
2018-10-09 11:32:40 +0530

    small change

--- WaySMS/src/test/java/tests/RealRunner.java
+++ WaySMS/src/test/java/tests/RealRunner.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features={"C:\\Users\\Naidu\\TestingWorkSpace\\WaySMS\\src\\test\\resources\\PropertiesResource\\feature1.feature",
 		"C:\\Users\\Naidu\\TestingWorkSpace\\WaySMS\\src\\test\\resources\\PropertiesResource\\feature2.feature"},
-		plugin= {"pretty","html:target"}, monochrome=true)
+		plugin= {"pretty","html:target"},monochrome=true)
 public class RealRunner
 {
 }
